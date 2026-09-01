078e2dc547e2b5a23d13abee47e9bd1197c50517
barcu00 <123418972+barcu00@users.noreply.github.com>
2026-09-01 15:45:31 +0200

    feat: load persistent OpenDash configuration

--- src/app/app.h
+++ src/app/app.h
@@ -2,6 +2,8 @@
 
 #include "board/board_display.h"
 #include "can/can_driver.h"
+#include "settings/app_config.h"
+#include "settings/nvs_config_store.h"
 #include "telemetry/data_source_manager.h"
 #include "telemetry/parameter_registry.h"
 #include "telemetry/vehicle_state.h"
@@ -15,12 +17,15 @@ public:
 private:
     VehicleState legacyVehicleState() const;
     TelemetryRuntimeStatus telemetryRuntimeStatus() const;
+    void applyConfig();
 
     BoardDisplay board_;
     ParameterRegistry registry_;
     DataSourceManager data_source_{registry_};
     CanDriver can_;
     Ui ui_;
+    AppConfig config_ = AppConfig::defaults();
+    NvsConfigStore config_store_;
     bool ready_ = false;
     uint32_t last_ui_update_ms_ = 0;
 };
