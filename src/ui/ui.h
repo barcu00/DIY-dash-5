f4818c2ddc316581c7fe14cbdb0c6cb67596a414
barcu00 <123418972+barcu00@users.noreply.github.com>
2026-09-01 16:55:44 +0200

    feat: prepare UI for configurable tile profiles

--- src/ui/ui.h
+++ src/ui/ui.h
@@ -1,10 +1,14 @@
 #pragma once
 
+#include <array>
 #include <cstdint>
 #include <lvgl.h>
 
 #include "board/board_display.h"
+#include "settings/app_config.h"
+#include "settings/nvs_config_store.h"
 #include "telemetry/data_source.h"
+#include "telemetry/parameter_registry.h"
 #include "telemetry/vehicle_state.h"
 #include "ui/gesture_navigation.h"
 
@@ -19,23 +23,50 @@ struct TelemetryRuntimeStatus {
 
 class Ui {
 public:
-    void begin();
+    void begin(AppConfig& config, ParameterRegistry& registry, NvsConfigStore& config_store);
     void update(const VehicleState& state, const RuntimeDiagnostics& diag,
                 const TelemetryRuntimeStatus& telemetry);
 
 private:
+    struct TileView {
+        lv_obj_t* container = nullptr;
+        lv_obj_t* title = nullptr;
+        lv_obj_t* value = nullptr;
+        lv_obj_t* icon = nullptr;
+        std::array<lv_color_t, 16U * 16U> icon_buffer{};
+        uint8_t config_index = 0U;
+        bool track = false;
+    };
+
     static void gestureEvent(lv_event_t* event);
-    static lv_obj_t* createValueTile(lv_obj_t* parent, const char* title, int x, int y, int w, int h,
-                                     lv_obj_t** value_label, lv_color_t accent);
+    static void tileEvent(lv_event_t* event);
+
+    static void createShiftBar(lv_obj_t* parent, lv_obj_t** segments);
     void createDash();
     void createDiag();
     void createTrack();
     void createSettings();
-    void createShiftBar(lv_obj_t* parent, lv_obj_t** segments);
+    void createConfigurableTiles(lv_obj_t* parent,
+                                 std::array<TileView, AppConfig::kTileCount>& views,
+                                 bool track);
+    void refreshTileLayout(std::array<TileView, AppConfig::kTileCount>& views,
+                           const std::array<TileConfig, AppConfig::kTileCount>& config);
+    void refreshTileValues(std::array<TileView, AppConfig::kTileCount>& views,
+                           const std::array<TileConfig, AppConfig::kTileCount>& config);
+    void renderTileIcon(TileView& view, const TileConfig& config);
+    void refreshAllTiles();
+    void openTileEditor(TileView& view);
+    void closeTileEditor();
+    void saveConfig();
+
     void load(UiPage page);
-    void updateShiftBar(lv_obj_t** segments, uint16_t rpm);
+    static void updateShiftBar(lv_obj_t** segments, uint16_t rpm);
 
     static Ui* instance_;
+
+    AppConfig* config_ = nullptr;
+    ParameterRegistry* registry_ = nullptr;
+    NvsConfigStore* config_store_ = nullptr;
 
     UiPage current_page_ = UiPage::Dash;
     lv_obj_t* dash_ = nullptr;
@@ -45,18 +76,9 @@ private:
 
     lv_obj_t* dash_rpm_ = nullptr;
     lv_obj_t* dash_gear_ = nullptr;
-    lv_obj_t* dash_speed_ = nullptr;
-    lv_obj_t* dash_map_ = nullptr;
-    lv_obj_t* dash_lambda_ = nullptr;
-    lv_obj_t* dash_clt_ = nullptr;
-    lv_obj_t* dash_iat_ = nullptr;
-    lv_obj_t* dash_oil_p_ = nullptr;
-    lv_obj_t* dash_oil_t_ = nullptr;
-    lv_obj_t* dash_fuel_p_ = nullptr;
-    lv_obj_t* dash_batt_ = nullptr;
-    lv_obj_t* dash_tps_ = nullptr;
     lv_obj_t* dash_source_ = nullptr;
     lv_obj_t* dash_shift_[12]{};
+    std::array<TileView, AppConfig::kTileCount> dash_tiles_{};
 
     lv_obj_t* diag_status_ = nullptr;
     lv_obj_t* diag_memory_ = nullptr;
@@ -68,10 +90,9 @@ private:
 
     lv_obj_t* track_rpm_ = nullptr;
     lv_obj_t* track_gear_ = nullptr;
-    lv_obj_t* track_speed_ = nullptr;
-    lv_obj_t* track_map_ = nullptr;
-    lv_obj_t* track_lambda_ = nullptr;
-    lv_obj_t* track_oil_p_ = nullptr;
-    lv_obj_t* track_clt_ = nullptr;
     lv_obj_t* track_shift_[12]{};
+    std::array<TileView, AppConfig::kTileCount> track_tiles_{};
+
+    lv_obj_t* tile_editor_overlay_ = nullptr;
+    TileView* editing_tile_ = nullptr;
 };
