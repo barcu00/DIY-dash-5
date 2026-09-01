5b635a38677ac20026841640188fd5ce02a42353
barcu00 <123418972+barcu00@users.noreply.github.com>
2026-09-01 19:13:28 +0200

    fix: increase LVGL memory pool for v0.2 UI

--- src/lv_conf.h
+++ src/lv_conf.h
@@ -8,7 +8,7 @@
 #define LV_COLOR_DEPTH 16
 #define LV_COLOR_16_SWAP 0
 #define LV_MEM_CUSTOM 0
-#define LV_MEM_SIZE (96U * 1024U)
+#define LV_MEM_SIZE (192U * 1024U)
 #define LV_TICK_CUSTOM 0
 #define LV_USE_LOG 0
 #define LV_USE_ASSERT_NULL 1
