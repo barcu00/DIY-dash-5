92234956720c2f874c033b166cf27789023ec7ae
barcu00 <123418972+barcu00@users.noreply.github.com>
2026-09-08 10:43:07 +0200

    test: define extended CAN profile mappings

--- test/test_can_profile_fixtures/test_main.cpp
+++ test/test_can_profile_fixtures/test_main.cpp
@@ -56,6 +56,29 @@ void test_ecumaster_core_frames_decode_to_native_units() {
     assertValue(status, ParameterId::BatteryVoltage, 13.5f);
 }
 
+void test_ecumaster_extended_engine_channels_decode_to_native_units() {
+    VehicleState combustion = decode(
+        "ecumaster_emu_black",
+        CanFrame{0x603U, 8U,
+                 {20U, 0U, 128U, 0U, 0x84U, 0x03U, 0xB6U, 0x03U},
+                 false, false});
+    assertValue(combustion, ParameterId::IgnitionTiming, 10.0f);
+    assertValue(combustion, ParameterId::Egt1, 900.0f);
+    assertValue(combustion, ParameterId::Egt2, 950.0f);
+
+    VehicleState status = decode(
+        "ecumaster_emu_black",
+        CanFrame{0x604U, 8U, {3U, 0U, 0xF4U, 0x01U, 0U, 0U, 0U, 85U},
+                 false, false});
+    assertValue(status, ParameterId::EthanolContent, 85.0f);
+
+    VehicleState target = decode(
+        "ecumaster_emu_black",
+        CanFrame{0x607U, 8U, {150U, 0U, 0U, 0U, 0U, 0U, 0U, 0U},
+                 false, false});
+    assertValue(target, ParameterId::BoostTarget, 1.5f);
+}
+
 void test_rusefi_core_frames_decode_to_native_units() {
     VehicleState speeds = decode(
         "rusefi_verbose",
@@ -81,6 +104,39 @@ void test_rusefi_core_frames_decode_to_native_units() {
     assertValue(fueling, ParameterId::FuelPressure, 1.0f, 0.001f);
 }
 
+void test_rusefi_extended_channels_decode_to_native_units() {
+    VehicleState fast = decode(
+        "rusefi_verbose",
+        CanFrame{0x201U, 8U,
+                 {0x94U, 0x11U, 0xE8U, 0x03U, 100U, 0U, 123U, 85U},
+                 false, false});
+    assertValue(fast, ParameterId::IgnitionTiming, 20.0f);
+    assertValue(fast, ParameterId::InjectorDuty, 50.0f);
+    assertValue(fast, ParameterId::EthanolContent, 85.0f);
+
+    VehicleState fueling = decode(
+        "rusefi_verbose",
+        CanFrame{0x205U, 8U,
+                 {0U, 0U, 0x10U, 0x0EU, 0x84U, 0x03U, 0U, 0U},
+                 false, false});
+    assertValue(fueling, ParameterId::MassAirFlow, 10.0f, 0.001f);
+    assertValue(fueling, ParameterId::InjectorPulseWidth, 3.0f, 0.001f);
+
+    VehicleState lambda = decode(
+        "rusefi_verbose",
+        CanFrame{0x207U, 8U,
+                 {0x10U, 0x27U, 0x28U, 0x23U, 0xB8U, 0x0BU, 0U, 0U},
+                 false, false});
+    assertValue(lambda, ParameterId::Lambda2, 0.9f);
+
+    VehicleState egt = decode(
+        "rusefi_verbose",
+        CanFrame{0x209U, 8U, {160U, 161U, 162U, 163U, 164U, 165U, 166U, 167U},
+                 false, false});
+    assertValue(egt, ParameterId::Egt1, 800.0f);
+    assertValue(egt, ParameterId::Egt8, 835.0f);
+}
+
 void test_maxxecu_core_frames_decode_to_native_units() {
     VehicleState fast = decode(
         "maxxecu_default_1_3",
@@ -100,6 +156,63 @@ void test_maxxecu_core_frames_decode_to_native_units() {
     assertValue(slow, ParameterId::Gear, 3.0f);
     assertValue(slow, ParameterId::OilPressure, 4.0f);
     assertValue(slow, ParameterId::OilTemperature, 100.0f);
+}
+
+void test_maxxecu_documented_extended_channels_decode_to_native_units() {
+    VehicleState fast2 = decode(
+        "maxxecu_default_1_3",
+        CanFrame{0x521U, 8U,
+                 {0xE8U, 0x03U, 0x84U, 0x03U, 200U, 0U, 0U, 0U},
+                 false, false});
+    assertValue(fast2, ParameterId::Lambda2, 0.9f);
+    assertValue(fast2, ParameterId::IgnitionTiming, 20.0f);
+
+    VehicleState injection = decode(
+        "maxxecu_default_1_3",
+        CanFrame{0x522U, 8U,
+                 {0x2CU, 0x01U, 0xF4U, 0x01U, 0U, 0U, 0xE8U, 0x03U},
+                 false, false});
+    assertValue(injection, ParameterId::InjectorPulseWidth, 3.0f);
+    assertValue(injection, ParameterId::InjectorDuty, 50.0f);
+
+    VehicleState sensors = decode(
+        "maxxecu_default_1_3",
+        CanFrame{0x530U, 8U,
+                 {0x64U, 0x05U, 0xF4U, 0x03U, 0x2CU, 0x01U, 0x84U, 0x03U},
+                 false, false});
+    assertValue(sensors, ParameterId::BarometricPressure, 1.012f);
+
+    VehicleState ethanol_egt = decode(
+        "maxxecu_default_1_3",
+        CanFrame{0x531U, 8U,
+                 {0U, 0U, 0x52U, 0x03U, 0U, 0U, 0x84U, 0x03U},
+                 false, false});
+    assertValue(ethanol_egt, ParameterId::EthanolContent, 85.0f);
+    assertValue(ethanol_egt, ParameterId::Egt1, 900.0f);
+
+    VehicleState egt2_5 = decode(
+        "maxxecu_default_1_3",
+        CanFrame{0x532U, 8U,
+                 {0x8EU, 0x03U, 0x98U, 0x03U, 0xA2U, 0x03U, 0xACU, 0x03U},
+                 false, false});
+    assertValue(egt2_5, ParameterId::Egt2, 910.0f);
+    assertValue(egt2_5, ParameterId::Egt5, 940.0f);
+
+    VehicleState egt6_8 = decode(
+        "maxxecu_default_1_3",
+        CanFrame{0x533U, 8U,
+                 {0xB6U, 0x03U, 0xC0U, 0x03U, 0xCAU, 0x03U, 0U, 0U},
+                 false, false});
+    assertValue(egt6_8, ParameterId::Egt6, 950.0f);
+    assertValue(egt6_8, ParameterId::Egt8, 970.0f);
+
+    VehicleState pressures = decode(
+        "maxxecu_default_1_3",
+        CanFrame{0x537U, 8U,
+                 {0xB8U, 0x0BU, 0U, 0U, 0x4CU, 0x04U, 0xDCU, 0x05U},
+                 false, false});
+    assertValue(pressures, ParameterId::CoolantPressure, 1.1f);
+    assertValue(pressures, ParameterId::BoostTarget, 1.5f);
 }
 
 void test_haltech_and_speeduino_compatible_frames_decode_big_endian() {
@@ -122,6 +235,32 @@ void test_haltech_and_speeduino_compatible_frames_decode_big_endian() {
     assertValue(lambda, ParameterId::Lambda, 1.0f);
 }
 
+void test_haltech_family_extended_broadcast_channels_decode() {
+    for (const char* profile : {"haltech_broadcast_2_0", "speeduino_haltech"}) {
+        VehicleState injection = decode(
+            profile,
+            CanFrame{0x362U, 8U,
+                     {0x01U, 0xF4U, 0U, 0U, 0x00U, 0xC8U, 0U, 0U},
+                     false, false});
+        assertValue(injection, ParameterId::InjectorDuty, 50.0f);
+        assertValue(injection, ParameterId::IgnitionTiming, 20.0f);
+
+        VehicleState pulse = decode(
+            profile,
+            CanFrame{0x364U, 8U,
+                     {0x0BU, 0xB8U, 0U, 0U, 0U, 0U, 0U, 0U},
+                     false, false});
+        assertValue(pulse, ParameterId::InjectorPulseWidth, 3.0f);
+
+        VehicleState mixture = decode(
+            profile,
+            CanFrame{0x368U, 8U,
+                     {0x03U, 0xE8U, 0x03U, 0x84U, 0U, 0U, 0U, 0U},
+                     false, false});
+        assertValue(mixture, ParameterId::Lambda2, 0.9f);
+    }
+}
+
 void test_link_indexed_frames_require_the_discriminator() {
     VehicleState rpm = decode(
         "link_generic_dash_experimental",
@@ -137,6 +276,31 @@ void test_link_indexed_frames_require_the_discriminator() {
         0x3E8U, 8U, {0U, 1U, 0x94U, 0x11U, 0U, 0U, 0U, 0U}, false, false};
     TEST_ASSERT_FALSE(decoder.decode(wrong_reserved, rejected, 1U));
     TEST_ASSERT_FALSE(rejected.get(ParameterId::Rpm).valid);
+}
+
+void test_link_experimental_profile_exposes_documented_extra_channels() {
+    VehicleState airflow = decode(
+        "link_generic_dash_experimental",
+        CanFrame{0x3E8U, 8U, {3U, 0U, 0U, 0U, 0U, 0U, 0xE8U, 0x03U},
+                 false, false});
+    assertValue(airflow, ParameterId::MassAirFlow, 100.0f);
+
+    VehicleState rear_wheels = decode(
+        "link_generic_dash_experimental",
+        CanFrame{0x3E8U, 8U,
+                 {9U, 0U, 0xE8U, 0x03U, 0xF2U, 0x03U, 0xFCU, 0x03U},
+                 false, false});
+    assertValue(rear_wheels, ParameterId::WheelSpeedLr, 100.0f);
+    assertValue(rear_wheels, ParameterId::WheelSpeedRf, 101.0f);
+    assertValue(rear_wheels, ParameterId::WheelSpeedRr, 102.0f);
+
+    VehicleState driver = decode(
+        "link_generic_dash_experimental",
+        CanFrame{0x3E8U, 8U,
+                 {13U, 0U, 0xF4U, 0x01U, 0x52U, 0x03U, 0U, 0U},
+                 false, false});
+    assertValue(driver, ParameterId::AcceleratorPosition, 50.0f);
+    assertValue(driver, ParameterId::EthanolContent, 85.0f);
 }
 
 void test_psa_profile_decodes_only_corroborated_engine_sensors() {
@@ -197,10 +361,15 @@ void test_out_of_range_signal_rejects_the_whole_matching_frame() {
 int main(int, char**) {
     UNITY_BEGIN();
     RUN_TEST(test_ecumaster_core_frames_decode_to_native_units);
+    RUN_TEST(test_ecumaster_extended_engine_channels_decode_to_native_units);
     RUN_TEST(test_rusefi_core_frames_decode_to_native_units);
+    RUN_TEST(test_rusefi_extended_channels_decode_to_native_units);
     RUN_TEST(test_maxxecu_core_frames_decode_to_native_units);
+    RUN_TEST(test_maxxecu_documented_extended_channels_decode_to_native_units);
     RUN_TEST(test_haltech_and_speeduino_compatible_frames_decode_big_endian);
+    RUN_TEST(test_haltech_family_extended_broadcast_channels_decode);
     RUN_TEST(test_link_indexed_frames_require_the_discriminator);
+    RUN_TEST(test_link_experimental_profile_exposes_documented_extra_channels);
     RUN_TEST(test_psa_profile_decodes_only_corroborated_engine_sensors);
     RUN_TEST(test_wrong_dlc_and_format_are_rejected_without_partial_update);
     RUN_TEST(test_out_of_range_signal_rejects_the_whole_matching_frame);
