054871a7a9117288a3a9a351aaa741a1f7f56c85
barcu00 <123418972+barcu00@users.noreply.github.com>
2026-09-18 10:28:17 +0200

    test(display): require full RGB frame storage for atomic presentation

--- test/test_display_tuning/test_main.cpp
+++ test/test_display_tuning/test_main.cpp
@@ -2,13 +2,12 @@
 
 #include "board/display_tuning.h"
 
-void test_lvgl_draw_buffer_uses_eighty_lines() {
-    TEST_ASSERT_EQUAL_UINT16(80U, DisplayTuning::kBufferLines);
-    TEST_ASSERT_EQUAL_UINT32(64000U, DisplayTuning::bufferPixels(800U));
+void test_rgb_buffer_holds_one_complete_frame_for_atomic_swap() {
+    TEST_ASSERT_EQUAL_UINT32(384000U, DisplayTuning::bufferPixels(800U));
 }
 
 int main(int, char**) {
     UNITY_BEGIN();
-    RUN_TEST(test_lvgl_draw_buffer_uses_eighty_lines);
+    RUN_TEST(test_rgb_buffer_holds_one_complete_frame_for_atomic_swap);
     return UNITY_END();
 }
