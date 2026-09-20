cfc8b3d09be0d1f136d5221b4e53a5c68b9d727d
barcu00 <123418972+barcu00@users.noreply.github.com>
2026-09-20 16:58:14 +0200

    test: remove temporary size probe

--- test/test_app_config/test_main.cpp
+++ test/test_app_config/test_main.cpp
@@ -220,15 +220,8 @@ void test_validation_rejects_unsnapped_or_out_of_range_shift_values() {
     TEST_ASSERT_FALSE(above_range.validate().valid);
 }
 
-void test_reports_persisted_blob_size() {
-    TEST_ASSERT_EQUAL_UINT32_MESSAGE(
-        0U, static_cast<uint32_t>(sizeof(AppConfig)),
-        "CONFIG DIAG: actual value is persisted blob size");
-}
-
 int main(int, char**) {
     UNITY_BEGIN();
-    RUN_TEST(test_reports_persisted_blob_size);
     RUN_TEST(test_defaults_define_approved_dash_and_track_slots);
     RUN_TEST(test_tiles_default_and_normalize_to_yellow_flag_color);
     RUN_TEST(test_validation_preserves_dormant_numeric_settings_on_flag_tiles);
